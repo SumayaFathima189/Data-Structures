c71a21c5eda9d50ce1937326f05e03de599e1230
SumayaFathima189 <sumayafathima5624@gmail.com>
2025-06-17 02:33:22 +0530

    commit

---
+++ prime_number.py
@@ -0,0 +1,29 @@
+# import math
+# number = int(input("enter a number to chcek whether it is prime or not - "))
+# if number>1:
+#     for x in range(2,int(math.sqrt(number))+1):
+#         if (number % x) == 0:
+#             print(f"{number} is not a prime number")
+#             break
+#     else:
+#         print(f"{number} is a prime number")
+
+# def func(n):
+#     sum=0
+#     for i in range(1,n+1):
+#         for j in range(1,i+1):
+#             sum+=1
+#     return sum
+# print(func(4))
+
+# def onsquretime(n):
+#     iteration  = 0
+#     for i in range(0,n):
+#         for j in range(0,n):
+#             print("*",end=" ")
+#             iteration+=1
+#         print("")
+#     print("\nwhen n is ",n,"ith = ",iteration,"\n")
+# onsquretime(5)
+# onsquretime(4)
+# onsquretime(3)
---
+++ square_it_out.py
@@ -0,0 +1,22 @@
+# def square_root(start_range,end_range):
+#     square_values = []
+#     odd_values = []
+#     even_values = []
+#     for i in range(start_range,end_range+1):
+#         square = i **2
+#         square_values.append(square)
+#         if square%2 == 0:
+#             even_values.append(square)
+#         else:
+#             odd_values.append(square)
+#     print(f"Range Starts from {start_range} to End {end_range}")
+#     print(f"Square Root List - {square_values}")
+#     print(f"Even Square Values - {even_values}")
+#     print(f"Odd Square Values - {odd_values}")
+
+
+
+# start_range = int(input("Enter the start range:"))
+# end_range = int(input("Enter the end range:"))
+# square_root(start_range,end_range)
+
